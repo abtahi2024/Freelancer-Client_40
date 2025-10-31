a07bceb5067a5937154c85f4bddcffe533ef1bc9
Abtahi chowdhury <abtahejisan@gmail.com>
2025-10-31 14:25:27 +0600

    akon jani naa

--- vite.config.js
+++ vite.config.js
@@ -2,24 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    outDir: "dist",
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return "vendor";
-          }
-        },
-      },
-    },
-  },
-  server: {
-    port: 5173,
-    open: true,
-  },
-  base: "./", // important for relative paths in production
 });

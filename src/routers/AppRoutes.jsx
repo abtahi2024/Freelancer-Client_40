0a0ff97b0594c6d89de79336f223ee70fff81b05
Abtahi chowdhury <abtahejisan@gmail.com>
2025-10-31 11:07:02 +0600

    capitaleWord

--- src/routers/AppRoutes.jsx
+++ src/routers/AppRoutes.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router";
 import MainLayout from "../layouts/MainLayout";
-import Home from "../pages/home";
+import Home from "../pages/Home";
 import ShopService from "../pages/ShopService";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
